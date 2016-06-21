8f6f72c17b36e9e707e582d2e65c2ea5e3326304
beam <patcharee4527@gmail.com>
2016-06-21 11:21:54 +0700

    Design Ui Main

---
+++ app/src/main/java/neu/kamonnit/bookshop/MyAlert.java
@@ -0,0 +1,30 @@
+package neu.kamonnit.bookshop;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by iMac_08 on 6/20/2016 AD.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTile,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle(strTile);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("จร้า", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+
+} // Main Class
--- app/src/main/java/neu/kamonnit/bookshop/SignUpActivity.java
+++ app/src/main/java/neu/kamonnit/bookshop/SignUpActivity.java
@@ -2,12 +2,89 @@ package neu.kamonnit.bookshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.internal.http.OkHeaders;
+
+import java.io.IOException;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private EditText nameEditText, userEditText, passwordEditText;
+    private String nameString, userString, passwordString;
+    private static final String urlPHP = "http://swiftcodingthai.com/neu/add_user_master.php";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText3);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText);
+
+    }   // Main Method
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("")  || userString.equals("")  || passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง คะ");
+
+        } else {
+            //No Space
+            uploadToServer();
+
+
+        }
+
+
+
+
+    }   // clickSign
+
+    private void uploadToServer() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                finish();
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+            }
+        });
+
+    }   //upload
+
+}   // Main Class
